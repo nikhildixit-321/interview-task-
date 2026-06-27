100da8c7c3f78dcfb1dfaffc0103a9b0c6f38aa0
Nikhil Dixit <170299140+nikhildixit-321@users.noreply.github.com>
2026-06-27 13:56:06 +0530

    vercel-problem

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true
+}));
 
 
 connectDB();
